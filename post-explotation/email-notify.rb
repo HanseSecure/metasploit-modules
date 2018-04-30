d667d4403ba7d9aab6b1791dcfc28d45adb72944
Florian Hansemann <info@hansesecure.de>
2018-04-30 12:35:11 +0200

    Add files via upload

---
+++ post-explotation/email-notify.rb
@@ -0,0 +1,29 @@
+# Content: Metasploit Modul to send email notification if a session is opened
+# Author: Florian Hansemann | @HanseSecure | https://hansesecure.de
+# Date: 05/2018
+
+
+require 'msf/core'
+
+class MetasploitModule < Msf::Post
+
+def initialize
+        super(
+        'Name' => 'Email Notification for opened session',
+        'Version' => '1.0',
+        'Description' => 'This Modul will notify you with an email if a session is opened',
+        'Author' => 'Florian Hansemann <info@hanseSecure.de>',
+        'License' => MSF_LICENSE,
+        'Platform' => 'multi')
+
+end
+
+def run
+
+begin
+
+system("python $Path2File/msf-email.py")
+
+end
+end
+end
---
+++ post-explotation/msf-email.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# Content:	Email notifying by MSF
+# Author: 	Florian Hansemann | @HanseSecure | https://hansesecure.de
+# Date: 	04/2018
+#
+
+import smtplib
+
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+
+
+def send():
+	#	Config here
+	yourMail = <yourMail>
+	smtpServer = <yourServer>
+	smtpPort = <yourServerPort>
+	yourPassword = <yourPassword>
+	
+        msg = MIMEText("Hey User, \nYour session is opened, the backdoor is installed and now it's time for Admin-Hunting!\n\nCheers\nYour C&C-Server")
+        msg['From'] = yourMail
+        msg['To'] = yourMail
+        msg['Subject'] = "Session opened"
+
+
+        #	Connect to SMTP-Server
+        smtpServer = smtplib.SMTP(smtpServer, smtpPort)
+        smtpServer.ehlo()
+        smtpServer.starttls()
+        smtpServer.ehlo()
+        smtpServer.login(yourMail, yourPassword)
+        smtpServer.sendmail(msg['From'], msg['To'], msg.as_string())
+        smtpServer.close()
+
+def main():
+    send()
+
+
+if __name__ == '__main__':
+    main()
